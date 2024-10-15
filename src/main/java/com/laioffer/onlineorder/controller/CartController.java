d07b03ec20e8b0988f806a770ade344044f62f57
Albert-Song443 <peiyan_song@brown.edu>
2024-10-15 00:19:26 -0400

    FilterChain

--- src/main/java/com/laioffer/onlineorder/controller/CartController.java
+++ src/main/java/com/laioffer/onlineorder/controller/CartController.java
@@ -1,11 +1,17 @@
 package com.laioffer.onlineorder.controller;
 
 
+import com.laioffer.onlineorder.entity.CustomerEntity;
 import com.laioffer.onlineorder.model.AddToCartBody;
 import com.laioffer.onlineorder.model.CartDto;
 import com.laioffer.onlineorder.service.CartService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import com.laioffer.onlineorder.service.CustomerService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -13,28 +19,35 @@ public class CartController {
 
 
     private final CartService cartService;
+    private final CustomerService customerService;
 
 
-    public CartController(CartService cartService) {
+    public CartController(
+            CartService cartService,
+            CustomerService customerService
+    ) {
         this.cartService = cartService;
+        this.customerService = customerService;
     }
 
 
     @GetMapping("/cart")
-    @ResponseStatus(HttpStatus.CREATED)
-    public CartDto getCart() {
-        return cartService.getCart(1L);
+    public CartDto getCart(@AuthenticationPrincipal User user) {
+        CustomerEntity customer = customerService.getCustomerByEmail(user.getUsername());
+        return cartService.getCart(customer.id());
     }
 
 
     @PostMapping("/cart")
-    public void addToCart(@RequestBody AddToCartBody body) {
-        cartService.addMenuItemToCart(1L, body.menuId());
+    public void addToCart(@AuthenticationPrincipal User user, @RequestBody AddToCartBody body) {
+        CustomerEntity customer = customerService.getCustomerByEmail(user.getUsername());
+        cartService.addMenuItemToCart(customer.id(), body.menuId());
     }
 
 
     @PostMapping("/cart/checkout")
-    public void checkout() {
-        cartService.clearCart(1L);
+    public void checkout(@AuthenticationPrincipal User user) {
+        CustomerEntity customer = customerService.getCustomerByEmail(user.getUsername());
+        cartService.clearCart(customer.id());
     }
 }
